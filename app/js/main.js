290a4c174a24fd16cc82f56310282db7b229f1b6
AlexandrWalker <alexandr.d.walker@gmail.com>
2024-01-26 22:11:32 +0700

    build main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,16 @@
 (() => {
   document.addEventListener('DOMContentLoaded', () => {
 
-    
+    $('.project__slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      centerMode: true,
+      prevArrow: false,
+      nextArrow: '<button type="button" class="slick-next"><svg width="24" height="44" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 2l20 20L2 42" stroke="#2E2D2D" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg></button>'
+    });
+
 
   });
 })();
