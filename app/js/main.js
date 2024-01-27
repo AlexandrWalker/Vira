3d47e5971e63ef1000f2bb98af3ed59d24e4cad7
AlexandrWalker <alexandr.d.walker@gmail.com>
2024-01-28 04:49:36 +0700

    add block in main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,48 @@
 (() => {
   document.addEventListener('DOMContentLoaded', () => {
 
-    $('.project__slider').slick({
+    $('.case__slider').slick({
       infinite: true,
       slidesToShow: 1,
       slidesToScroll: 1,
       arrows: true,
       centerMode: true,
       prevArrow: false,
-      nextArrow: '<button type="button" id="project__slider__btn" class="slick-next"><svg width="24" height="44" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 2l20 20L2 42" stroke="#2E2D2D" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg></button>'
+      nextArrow: '<button type="button" id="case__slider__btn" class="slick-next"><svg width="24" height="44" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 2l20 20L2 42" stroke="#2E2D2D" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg></button>'
     });
 
     $('.stages__slider').slick({
       infinite: false,
       slidesToShow: 4,
       slidesToScroll: 1,
-      arrows: true
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" viewBox="0 0 10 16" fill="none"><path d="M8 2L2 8L8 14" stroke="#A7A7A7" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+      nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" viewBox="0 0 10 16" fill="none"><path d="M2 2L8 8L2 14" stroke="#A7A7A7" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg></button>'
     });
 
-    document.getElementById("project__tab1").addEventListener("click", function () {
-      document.getElementById("project__tab1").classList.add("active");
-      document.getElementById("project__tab2").classList.remove("active");
-
-      document.getElementById("project__slider1").classList.add("active");
-      document.getElementById("project__slider2").classList.remove("active");
+    $('.projects__slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" viewBox="0 0 10 16" fill="none"><path d="M8 2L2 8L8 14" stroke="#2E2D2D" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+      nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" viewBox="0 0 10 16" fill="none"><path d="M2 2L8 8L2 14" stroke="#2E2D2D" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg></button>'
     });
 
-    document.getElementById("project__tab2").addEventListener("click", function () {
-      document.getElementById("project__tab2").classList.add("active");
-      document.getElementById("project__tab1").classList.remove("active");
+    document.getElementById("case__tab1").addEventListener("click", function () {
+      document.getElementById("case__tab1").classList.add("active");
+      document.getElementById("case__tab2").classList.remove("active");
 
-      document.getElementById("project__slider1").classList.remove("active");
-      document.getElementById("project__slider2").classList.add("active");
+      document.getElementById("case__slider1").classList.add("active");
+      document.getElementById("case__slider2").classList.remove("active");
+    });
+
+    document.getElementById("case__tab2").addEventListener("click", function () {
+      document.getElementById("case__tab2").classList.add("active");
+      document.getElementById("case__tab1").classList.remove("active");
+
+      document.getElementById("case__slider1").classList.remove("active");
+      document.getElementById("case__slider2").classList.add("active");
     });
 
     (function () {
